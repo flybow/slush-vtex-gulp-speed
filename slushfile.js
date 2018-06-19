a827fc33956a1ab90e1d73d4a70d0f5cac0c52a9
Carlo Schneider <uab.carlo@gmail.com>
2018-06-19 18:19:12 -0300

    Fix corrupt images; Fix .gitignore

--- slushfile.js
+++ slushfile.js
@@ -18,11 +18,19 @@ gulp.task('default', function (done) {
     }
   ];
 
+  var filterImages = $.filter(['**/**', '!template/assets/img/', '!template/assets/img/**'], { restore: true });
+
   inquirer.prompt(questions)
     .then(function (answers) {
       gulp.src(__dirname + '/template/**', { dot: true })
+        .pipe(filterImages)
         .pipe($.template(answers, { interpolate: /<%=([\s\S]+?)%>/g }))
+        .pipe(filterImages.restore)
         .pipe($.rename(function (file) {
+          if (file.basename[0] === '@') {
+            file.basename = '.' + file.basename.slice(1)
+          }
+
           if (file.basename.indexOf('-acronym-') > -1) {
             file.basename = file.basename.replace('acronym', answers.storeAcronym);
           }
